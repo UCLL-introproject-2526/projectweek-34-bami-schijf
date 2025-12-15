05549224e433c0fc3e954f395e65ca1956c9fce4
John Doe <tim.vandelochttelenet@gmail.com>
2025-12-15 14:24:39 +0100

    permanent window

--- foobar.py
+++ foobar.py
@@ -3,3 +3,11 @@ import pygame
 
 # Initialize Pygame
 pygame.init()
+
+screen_size = (1024, 768)
+
+def create_main_window(size):
+    while True:
+        pygame.display.set_mode(size)
+
+create_main_window(screen_size)
