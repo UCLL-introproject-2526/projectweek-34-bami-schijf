d55a7e5f71e316d7a0455a173ee882961538c14c
Seppe Pauwels <seppe.pauwels@student.ucll.be>
2025-12-16 13:30:04 +0100

    tutorial message

--- blueprint.py
+++ blueprint.py
@@ -37,7 +37,7 @@ class Player:
         self.image = pygame.image.load("sprites/PPAP - sprite/PPAP - right.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
-    def get_rect(self): #CREATE COLLISION BOX PLAYER
+    def get_rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
 
 
@@ -45,10 +45,10 @@ class Npc:
     def __init__(self):
         self.x, self.y = randint(0, screen_size[0]), randint(0, screen_size[1])
         self.width, self.height = 45, 60
-        #CREATE COLLISION BOX NPC
         self.shrink_width = 22.5
         self.shrink_height = 45
-    def get_rect(self): #RETURN COLLISION BOX NPC
+
+    def get_rect(self):
         return pygame.Rect(
             self.x + self.shrink_width // 2,
             self.y + self.shrink_height // 2,
@@ -66,7 +66,7 @@ class Labubu(Npc):
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
-    def get_rect(self): #COLLISION BOX LABUBU
+    def get_rect(self):
         shrink_w, shrink_h = 30, 40
         return pygame.Rect(
             self.x + shrink_w // 2,
@@ -83,7 +83,8 @@ class Zombie(Npc):
             (0, 200, 0),
             (self.x, self.y, self.width, self.height)
         )
-    def get_rect(self): #COLLISION BOX ZOMBIE
+
+    def get_rect(self):
         shrink_w, shrink_h = 30, 40
         return pygame.Rect(
             self.x + shrink_w // 2,
@@ -92,6 +93,7 @@ class Zombie(Npc):
             self.height - shrink_h
         )
 
+
 class Fruit(Npc):
     def draw(self, screen):
         pygame.draw.rect(
@@ -99,7 +101,8 @@ class Fruit(Npc):
             (0, 0, 200),
             (self.x, self.y, self.width, self.height)
         )
-    def get_rect(self): #COLLISION BOX FRUIT
+
+    def get_rect(self):
         shrink_w, shrink_h = 5, 10
         return pygame.Rect(
             self.x + shrink_w // 2,
@@ -112,14 +115,14 @@ class Fruit(Npc):
 class Boss(Npc):
     def __init__(self):
         super().__init__()
-        self.x = screen_size[0] // 2 - self.width
-        self.y = screen_size[1] // 8
         self.width = 150
         self.height = 200
+        self.x = screen_size[0] // 2 - self.width
+        self.y = screen_size[1] // 8
         self.image = pygame.image.load("sprites/Labubu - sprite/Labubu - blue.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
-    def get_rect(self): #COLLISION BOX BOSS
+    def get_rect(self):
         shrink_w, shrink_h = 80, 120
         return pygame.Rect(
             self.x + shrink_w // 2,
@@ -132,14 +135,28 @@ class Boss(Npc):
         screen.blit(self.image, (self.x, self.y))
 
 
+class TutorialText:
+    def __init__(self):
+        self.width = 500
+        self.height = 200
+        self.image = pygame.image.load("background/tutorial gamecontrols.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.x = (screen_size[0] - self.width) // 2
+        self.y = (screen_size[1] // 2) + 50
 
-def renderFrame(screen, player: Player, npcs: list): #PHASE THROUGH ENEMIES (LAYERS)
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+
+def renderFrame(screen, player: Player, npcs: list, tutorial=None):
     screen.fill((0, 0, 0))
+    if tutorial:
+        tutorial.draw(screen)
     drawables = npcs + [player]
     drawables.sort(key=lambda obj: obj.y + obj.height)
     for obj in drawables:
         obj.draw(screen)
-    flip()
+
 
 def main():
     pygame.init()
@@ -148,7 +165,8 @@ def main():
     clock = pygame.time.Clock()
 
     player = Player()
-    running = True
+    tutorial = TutorialText()
+
     enemies = []
     for _ in range(3):
         enemies.append(Fruit())
@@ -158,13 +176,14 @@ def main():
         enemies.append(Zombie())
     enemies.append(Boss())
     
+    running = True
     while running:
         clock.tick(60)
         pygame.event.pump()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     player.look_right()
                 elif event.key == pygame.K_LEFT:
@@ -182,13 +201,15 @@ def main():
         if held[pygame.K_RIGHT]:
             player.right()
 
-        player_rect = player.get_rect() #COLLISION DETECTION
+        player_rect = player.get_rect()
         for npc in enemies:
             if player_rect.colliderect(npc.get_rect()):
                 player.x, player.y = old_x, old_y
                 break
-    
-        renderFrame(screen, player, enemies)
+
+        renderFrame(screen, player, enemies, tutorial)
+        pygame.display.flip()
+
 
     pygame.quit()
 
